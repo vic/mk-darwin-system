4ccc687328aa17f481de8052051fdb2cfbc32ca2
Victor Hugo Borja <vborja@apache.org>
2021-07-30 11:56:36 -0500

    Only show diff-closures when /run/current-system exists.
    On fist-time installations it's very likely that /run/current-system
    is not already present. Since there's no currently installed nix-darwin
    generation.
    
    Fix #1

--- default.nix
+++ default.nix
@@ -50,11 +50,13 @@
 
     activationDiffModule = { pkgs, config, ... }: {
       system.activationScripts.diffClosures.text = ''
-        echo "new configuration diff" >&2
-        $DRY_RUN_CMD ${pkgs.nixUnstable}/bin/nix store \
-            --experimental-features 'nix-command' \
-            diff-closures /run/current-system "$systemConfig" \
-            | sed -e 's/^/[diff]\t/' >&2
+        if [ -e /run/current-system ]; then
+          echo "new configuration diff" >&2
+          $DRY_RUN_CMD ${pkgs.nixUnstable}/bin/nix store \
+              --experimental-features 'nix-command' \
+              diff-closures /run/current-system "$systemConfig" \
+              | sed -e 's/^/[diff]\t/' >&2
+        fi
       '';
 
       system.activationScripts.preActivation.text =
