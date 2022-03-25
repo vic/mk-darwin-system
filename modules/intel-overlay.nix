1b197acf6078dfead9214d531980390cd2bdf685
Victor Hugo Borja <vborja@apache.org>
2022-03-24 23:36:35 -0600

    Niv now builds on m1

--- modules/intel-overlay.nix
+++ modules/intel-overlay.nix
@@ -9,7 +9,6 @@
 
       in if isSillicon then {
         # Common packages that are still not able to build on m1
-        inherit (intelPkgs) niv;
 
         # Marked as broken in aarch64-darwin
         inherit (intelPkgs)
@@ -18,18 +17,6 @@
 
       } else
         { })
-
-    # https://github.com/LnL7/nix-darwin/issues/417
-    (new: old: {
-      haskellPackages = old.haskellPackages.override {
-        overrides = self: super:
-          let
-            workaround140774 = hpkg:
-              with new.haskell.lib;
-              overrideCabal hpkg (drv: { enableSeparateBinOutput = false; });
-          in { niv = workaround140774 super.niv; };
-      };
-    })
 
   ];
 }
